ca2d67ee603b7a1cf2b3c2ded02fb6521ef0accc
Maksim Horbachevsky <fantactuka@gmail.com>
2010-08-02 10:39:15 +0300

    Added js docs

--- Source/Decorator.js
+++ Source/Decorator.js
@@ -1,4 +1,10 @@
 Function.implement({
+    /**
+     * Function method that adds decorator pattern.
+     * @method decorate
+     * @param decorator {Function} - function that will be used for decoration
+     * @return {Function} - decorated function
+     */
     decorate: function(decorator) {
         var method = this;
         return function() {
@@ -8,7 +14,24 @@ Function.implement({
 });
 
 Function.Decorators = {
-    StrictAccess: function() {
+    /**
+     * Decorate function with parameters validation.
+     *
+     * @method StrictArguments
+     * @params {Arguments} list of valid types of arguments and its amount
+     * @return {Function} - decorated function
+     *
+     * @requires $type function to validate arguments types
+     * @example
+     *
+     *      function(numParam, strParam, boolParam) {
+     *          ...
+     *      }.decorate(Function.Decorators.StrictAccess('number', 'string', 'boolean'))
+     *
+     *      Will require exact 3 arguments with 'number', 'string', 'boolean' types,
+     *      otherwise throw error message.
+     */
+    StrictArguments: function() {
         var types = arguments;
         return function(method, args) {
             if(types.length != args.length) {
@@ -23,6 +46,24 @@ Function.Decorators = {
         };
     },
 
+    /**
+     * Decorate function with returned value validation.
+     *
+     * @method StrictReturn
+     * @param type {String} - required type of returned value
+     * @return {Function} - decorated function
+     *
+     * @requires $type function to validate arguments types
+     * @example
+     *
+     *      function() {
+     *          ...
+     *          retrun result;
+     *      }.decorate(Function.Decorators.StrictReturn('number'))
+     *
+     *      Will require 'number' type value to be returned by the function,
+     *      otherwise throw error message.
+     */
     StrictReturn: function(type) {
         return function(method, args) {
             var result = method.apply(this, args);
@@ -34,6 +75,25 @@ Function.Decorators = {
         };
     },
 
+    /**
+     * Decorate function with trottle pattern: it allows to call function only once per `interval`
+     * other calls within this interval will be ignored.
+     *
+     * @method Throttle
+     * @param interval {Number} - interval in milliseconds
+     * @return {Function} - decorated function
+     *
+     * @example
+     *
+     *      function throttled() {
+     *          ...
+     *          retrun result;
+     *      }.decorate(Function.Decorators.Throttle(3000))
+     *
+     *      In case throttled() called it will run immediately and in case it will be called again within 3000ms
+     *      all these calls will be ignored.
+     *
+     */
     Throttle: function(interval) {
         var timer = null;
         return function(method, args) {
@@ -44,6 +104,23 @@ Function.Decorators = {
         };
     },
 
+    /**
+     * Decorate function with debounce pattern: it allows to slow down the function calls,
+     * in case debounced function is called it will run only after `interval`. In case function
+     * will be called within this interval it will clear old one and create new interval.
+     *
+     * @method Debounce
+     * @param interval {Number} - interval in milliseconds
+     * @return {Function} - decorated function
+     *
+     * @example
+     *
+     * It could be used with `auto-suggestion` that runs request after keypressed:
+     * Press some key, in case no other key pressed within 3000 ms it will make ajax request.
+     *
+     * $('auto-suggested-field').addEvent('keyup', sendRequest.decorate(Function.Decorators.Throttle(3000)));
+     *
+     */
     Debounce: function(interval) {
         var timer = null;
         return function(method, args) {
@@ -53,6 +130,29 @@ Function.Decorators = {
         };
     },
 
+
+    /**
+     * Decorate function with queue pattern: all function calls will be run one by one after `interval` ms.
+     *
+     * @method Queue
+     * @param interval {Number} - interval in milliseconds
+     * @return {Function} - decorated function
+     *
+     * @example
+     *
+     *      function queued() {
+     *          alert('call')
+     *      }.decorate(Function.Decorators.Queue(3000))
+     *
+     *      queued();
+     *      queued();
+     *      queued();
+     *
+     *      alert('call'); // Shown immediately
+     *      alert('call'); // After 3000 ms
+     *      alert('call'); // After 6000 ms
+     *
+     */
     Queue: function(interval) {
         var calls = [];
         var timer = null;
